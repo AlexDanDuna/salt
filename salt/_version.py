cef1778ba4a44f0e8ec6a54468e8a62e35216882
Raul Zavaczki <raul.zavaczki@ni.com>
2023-05-16 14:55:37 +0300

    change version

--- salt/_version.py
+++ salt/_version.py
@@ -2,4 +2,4 @@
 
 from salt.version import SaltStackVersion
 
-__saltstack_version__ = SaltStackVersion(3000, 2, None, 0, '', 0, 0, 'g064729c1ed')
+__saltstack_version__ = SaltStackVersion(3000, 2, None, 0, '', 0, 10, 'geffff1c00b')
